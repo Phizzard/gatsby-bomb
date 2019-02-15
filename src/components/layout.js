fedc90634815505694d140a94bfbf90c119467f0
Phil Tietjen <tietjen.philip@gmail.com>
2019-02-15 00:07:05 -0500

    small minor style changes to prepare

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,9 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import "./layout.css";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -22,25 +22,27 @@ const Layout = ({ children }) => (
         <div
           style={{
             margin: `0 auto`,
-            maxWidth: 960,
+            maxWidth: 1440,
             padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            paddingTop: 0
           }}
         >
           <main>{children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
+            © {new Date().getFullYear()} Philip Tietjen, Built with
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
+            {` & `}
+            <a href="https://www.giantbomb.com/api">GiantBomb API</a>
           </footer>
         </div>
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
