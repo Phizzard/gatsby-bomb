648f2237f584ed9b6b11c83b4a7e537e24b426c5
Phil Tietjen <ptietjen@labx.com>
2019-03-03 16:28:46 -0500

    enable offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,19 +34,19 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Gatsby Bomb`,
+        short_name: `GatsbyBomb`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        background_color: `#242628`,
+        theme_color: `#000`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
     },
     `gatsby-plugin-transition-link`,
-    `gatsby-plugin-emotion`
+    `gatsby-plugin-emotion`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline'
   ]
 };
